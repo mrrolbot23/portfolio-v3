9ee75cd836f19e540f6af35e0fcfa40a3b521240
Edwin Roldan Delgado <edwin@Edwins-MacBook-Pro.local>
2025-03-14 13:27:01 -0500

    Vitest setup complete

---
+++ portfoliov3/src/header/__tests__/Header.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render, screen} from "@testing-library/react"
+import {Header} from "../Header";
+import { describe, expect, it } from "vitest";
+
+describe('Header', () => {
+    it('should display header title', () => {
+        render(<Header/>);
+        expect(screen.getByText("My Header")).toBeVisible();
+    });
+});
